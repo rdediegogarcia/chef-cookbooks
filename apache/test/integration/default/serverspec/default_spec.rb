b84a61b6e9346a640ee431b601a175e531eb2df1
ChefDK User <chef@ip-10-180-253-173.ec2.internal>
2016-06-23 12:41:52 +0000

    Kitchen w Test

--- apache/test/integration/default/serverspec/default_spec.rb
+++ apache/test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,16 @@ require 'spec_helper'
 describe 'apache::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe package ('httpd') do
+     it { should be_installed }
   end
+
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  describe command('curl localhost') do
+     its(:exit_status) { should eq 0 }
+  end
+
 end
