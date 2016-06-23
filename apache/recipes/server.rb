2c4570d4120db471aa7279965e7f7bf99cf1012a
ChefDK User <chef@ip-10-180-253-173.ec2.internal>
2016-06-23 13:57:21 +0000

    ERB Templates

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -9,10 +9,8 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-    content "<h1>Hola Capullos! (created by Chef)</h1>
-             <h2>IPADDRESS: #{node['ipaddress']}</h2>
-             <h2>HOSTNAME : #{node['hostname']} </h2>"
+template '/var/www/html/index.html' do
+   source 'index.html.erb'
 end
 
 service 'httpd' do
